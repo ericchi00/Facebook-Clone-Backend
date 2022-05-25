6d96f76a64b7f145865acd4115b8cfc58c354664
ericchi00 <ericchi00@gmail.com>
2022-05-25 14:28:51 -0700

    moved password hash to model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const { Schema } = mongoose;
 
@@ -10,10 +11,28 @@ const userSchema = new Schema(
 		password: { type: String, required: true },
 		friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 		friendRequest: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 		picture: { type: Schema.Types.ObjectId, ref: 'Img' },
 	},
 	{ timestamps: true }
 );
+
+userSchema.pre('save', function (next) {
+	const user = this;
+	if (user.password) {
+		bcrypt.genSalt(10, function (err, salt) {
+			if (err) return next(err);
+
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				if (err) return next(err);
+
+				user.salt = salt;
+				user.password = hash;
+				next();
+			});
+		});
+	}
+});
 
 const User = mongoose.model('User', userSchema);
 
