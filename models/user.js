a52cdd84180825bd19caee263f80107af18d9351
ericchi00 <ericchi00@gmail.com>
2022-05-25 18:05:20 -0700

    added in hash/salt & name trimming

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
 	{
 		firstName: { type: String, required: true, minlength: 1 },
 		lastName: { type: String, required: true, minlength: 1 },
-		email: { type: String, required: true, minlength: 5 },
+		email: { type: String, unique: true, required: true, minlength: 5 },
 		password: { type: String, required: true },
 		friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 		friendRequest: [{ type: Schema.Types.ObjectId, ref: 'User' }],
@@ -17,21 +17,20 @@ const userSchema = new Schema(
 	{ timestamps: true }
 );
 
+userSchema.pre('save', async function () {
+	const hash = await bcrypt.hash(this.password, 10);
+	this.password = hash;
+});
+
 userSchema.pre('save', function (next) {
 	const user = this;
-	if (user.password) {
-		bcrypt.genSalt(10, function (err, salt) {
-			if (err) return next(err);
-
-			bcrypt.hash(user.password, salt, (err, hash) => {
-				if (err) return next(err);
-
-				user.salt = salt;
-				user.password = hash;
-				next();
-			});
-		});
-	}
+	user.firstName =
+		user.firstName.charAt(0).toUpperCase() +
+		user.firstName.slice(1).toLowerCase();
+	user.lastName =
+		user.lastName.charAt(0).toUpperCase() +
+		user.lastName.slice(1).toLowerCase();
+	next();
 });
 
 const User = mongoose.model('User', userSchema);
