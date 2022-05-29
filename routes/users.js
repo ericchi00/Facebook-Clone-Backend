34c3c9d168201c10b35881ccf10e106e24fe5d53
ericchi00 <ericchi00@gmail.com>
2022-05-29 13:49:17 -0700

    added in route for updating picture

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ import {
 	loginPost,
 	putUserInfo,
 	getProfileInfo,
+	putUserPicture,
 } from '../controllers/usercontroller.js';
 
 const router = Router();
@@ -17,6 +18,12 @@ router.put(
 	'/profile/:id',
 	passport.authenticate('jwt', { session: false }),
 	putUserInfo
+);
+
+router.put(
+	'/profile/picture/:id',
+	passport.authenticate('jwt', { session: false }),
+	putUserPicture
 );
 
 router.get(
