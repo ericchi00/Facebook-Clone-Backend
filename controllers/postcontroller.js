e598fc3218bec16444438975d968ae433cff0079
ericchi00 <ericchi00@gmail.com>
2022-05-29 17:52:19 -0700

    added in postuserpost & getallposts

---
+++ controllers/postcontroller.js
@@ -0,0 +1,38 @@
+import sanitizeHtml from 'sanitize-html';
+import { body, validationResult } from 'express-validator';
+import Post from '../models/post.js';
+
+const getAllPosts = async (req, res, next) => {
+	try {
+		const list = Post.find().populate({
+			path: 'comments',
+			populate: { path: 'name', select: ['firstName', 'lastName'] },
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+const postUserPost = [
+	body('userPost')
+		.isLength({ min: 1 })
+		.withMessage('Must be at least 1 character long'),
+	async (req, res, next) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json(errors);
+			}
+
+			const { userPost } = req.body;
+			await Post.create({
+				name: req.params.id,
+				text: userPost,
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
+];
+
+export { getAllPosts, postUserPost };
