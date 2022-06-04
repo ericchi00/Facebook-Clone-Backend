a9c0fbecaec8717d91537a1e79e2e420650e2d4a
ericchi00 <ericchi00@gmail.com>
2022-06-03 17:38:10 -0700

    populate comments from post

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,13 +1,17 @@
-import sanitizeHtml from 'sanitize-html';
 import { body, validationResult } from 'express-validator';
 import Post from '../models/post.js';
 
 const getAllPosts = async (req, res, next) => {
 	try {
-		const list = Post.find().populate({
-			path: 'comments',
-			populate: { path: 'name', select: ['firstName', 'lastName'] },
-		});
+		const list = await Post.find()
+			.populate('name', 'firstName lastName picture likes')
+			.populate({
+				path: 'comments',
+				populate: { path: 'name', select: 'firstName lastName _id picture' },
+				options: { sort: { createdAt: -1 } },
+			})
+			.sort([['createdAt', 'descending']]);
+		return res.status(200).json(list);
 	} catch (error) {
 		next(error);
 	}
@@ -16,6 +20,7 @@ const getAllPosts = async (req, res, next) => {
 const postUserPost = [
 	body('userPost')
 		.isLength({ min: 1 })
+		.trim()
 		.withMessage('Must be at least 1 character long'),
 	async (req, res, next) => {
 		try {
@@ -37,4 +42,20 @@ const postUserPost = [
 	},
 ];
 
-export { getAllPosts, postUserPost };
+const putPostLike = async (req, res, next) => {
+	try {
+		const { name } = req.body;
+		const post = await Post.findById(req.params.id);
+		if (post.likes.includes(name)) {
+			post.likes.pull(name);
+		} else {
+			post.likes.push(name);
+		}
+		post.save();
+		return res.status(200).json({ status: 'success' });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export { getAllPosts, postUserPost, putPostLike };
