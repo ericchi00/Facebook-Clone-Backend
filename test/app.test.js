02d69f27bae68e705a8e5999e8a7d7dab09d48fe
ericchi00 <ericchi00@gmail.com>
2022-06-05 13:30:04 -0700

    updated tests

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,7 @@ import app from '../app.js';
 let token = '';
 beforeAll(async () => {
 	token = jwt.sign(
-		{ id: '62901c6cecb70001f9935570', email: 'test@test.com' },
+		{ id: '62991a74a96aa84ad9dd9b6d', email: 'luffy@luffy.com' },
 		process.env.JWT_TOKEN,
 		{
 			expiresIn: '1h',
@@ -25,19 +25,26 @@ test('it should handle 404 error', (done) => {
 });
 
 test('it should respond with 401, unauthorized,', (done) => {
-	request(app).get('/users/profile/629006235a505c6db4f60107').expect(401, done);
+	request(app).get('/api/profile/62991a74a96aa84ad9dd9b6d').expect(401, done);
 });
 
-test('login returns 200', (done) => {
+test('login returns 500', (done) => {
 	request(app)
-		.post('/users/login')
+		.post('/auth/login')
 		.send({ email: 'test@test.com', password: 'test@test.com' })
-		.expect(200, done);
+		.expect(500, done);
 });
 
 test('it should return profile json data', (done) => {
 	request(app)
-		.get('/users/profile/629006235a505c6db4f60107')
+		.get('/api/profile/62991a74a96aa84ad9dd9b6d')
+		.set('Authorization', `Bearer ${token}`)
+		.expect(200, done);
+});
+
+test('it should return 200 when getting posts', (done) => {
+	request(app)
+		.get('/api/posts')
 		.set('Authorization', `Bearer ${token}`)
 		.expect(200, done);
 });
